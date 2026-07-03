5f2e540422d4c143c6b51778d2f512b1c43df6d6
the25thjin <mannr075@gmail.com>
2026-07-04 04:48:32 +0530

    typo mistake corrected

--- app/routers/signal_sources.py
+++ app/routers/signal_sources.py
@@ -5,21 +5,21 @@ from app.services.signal_source_service import create, update, find_all, find_by
 router = APIRouter(prefix="/signal-sources", tags=["Signal Sources"])
 
 @router.post("/", response_model=SignalSourceResponse)
-async def create_scoring_rule(request : SignalSourceCreate):
+async def create_signal_source(request : SignalSourceCreate):
     return await create(request)
 
 @router.get("/", response_model=list[SignalSourceResponse])
-async def find_all_scoring_rules():
+async def find_all_signal_sources():
     return await find_all()
 
 @router.get("/{id}", response_model=SignalSourceResponse)
-async def find_scoring_rule_by_id(id : str):
+async def find_signal_source_by_id(id : str):
     return await find_by_id(id)
 
 @router.put("/{id}", response_model=SignalSourceResponse)
-async def update_scoring_rule(id : str,request : SignalSourceUpdate):
+async def update_signal_source(id : str,request : SignalSourceUpdate):
     return await update(id,request)
 
 @router.delete("/{id}")
-async def delete_scoring_rule(id: str):
+async def delete_signal_source(id: str):
     return await delete(id)
