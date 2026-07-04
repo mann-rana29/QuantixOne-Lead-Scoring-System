28408e5e7da2d1dfb72ae77c050f088081f935ad
the25thjin <mannr075@gmail.com>
2026-07-04 21:48:01 +0530

    created root  GET endpoint

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from app.routers.personas import router as persona_router
 from app.routers.score_thresholds import router as score_threshold_router
 from app.routers.scoring_rules import router as scoring_rule_router
 from app.routers.signal_sources import router as signal_source_router
+from app.routers.lead_signals import router as lead_signal_router
 
 @asynccontextmanager
 async def lifespan(app : FastAPI):
@@ -21,6 +22,11 @@ app.include_router(persona_router)
 app.include_router(score_threshold_router)
 app.include_router(scoring_rule_router)
 app.include_router(signal_source_router)
+app.include_router(lead_signal_router)
+
+@app.get("/")
+async def root():
+    return {"message" : "backend is live"}
 
 @app.get("/health")
 async def health():
